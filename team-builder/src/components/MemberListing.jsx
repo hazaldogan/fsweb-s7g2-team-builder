812e530bafab54f14849e5a43ab6b1feea8d41b7
Hazal Taştan <hazl.dogann@gmail.com>
2023-10-03 17:17:27 +0300

    memberList component oluşturuldu

---
+++ team-builder/src/components/MemberListing.jsx
@@ -0,0 +1,21 @@
+
+
+function MemberListing(props) {
+  return (
+    <>
+      <h3>Üyelerimiz</h3>
+      <ul>
+        {props.memberList.map((member,i) => {
+          return (
+            <li key={i}>
+              <a href={`mailto:${member.email}`}>{member.isim}</a> (
+              {member.rol})
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default MemberListing;
